2958ffdc8f9629df03fb96c88318bc7ad7fc51c7
almirmlc <almir.mulalic@bulme.at>
2022-01-27 12:55:18 +0100

    Abgabe

--- gpio.cpp
+++ gpio.cpp
@@ -1,6 +1,41 @@
+#include <lgpio.h>
 #include "gpio.h"
+
 
 Gpio::Gpio(QObject *parent) : QObject(parent)
 {
+    m_handle = lgGpiochipOpen(0);
+    int init_lvl = 0;
+    for(int pin : LEDS)
+        lgGpioClaimOutput(m_handle, LFLAGS, pin, init_lvl);
+}
 
+
+Gpio::~Gpio()
+{
+    int state = 0;
+    for (int pin : LEDS)
+        lgGpioWrite(m_handle, pin, state);
+
+    lgGpiochipClose(m_handle);
+}
+
+
+void Gpio::set(int pin,bool value)
+{
+    lgGpioWrite(m_handle, pin, value);
+}
+
+
+void Gpio::set(unsigned int pattern)
+{
+    int n = 0;
+    bool value;
+    unsigned int proof = 0b0001;
+    for(auto pin : LEDS)
+    {
+        value = proof & pattern >> n;
+        lgGpioWrite(m_handle, pin, value);
+        n++;
+    }
 }
--- gui.cpp
+++ gui.cpp
@@ -1,22 +1,61 @@
 #include "gui.h"
 
-Gui::Gui(QWidget *parent) :
-    QWidget(parent)
+Gui::Gui(QWidget *parent) : QWidget(parent)
 {
     setupUi(this);
+    m_leds = new Gpio(this);
+    m_state = 0;
+    m_timer = new QTimer();
+    connect(m_timer, &QTimer::timeout, this, &Gui::toggle);
+    m_timer->start(TIMEOUT);
 }
+
 
 void Gui::on_speedSlider_valueChanged(int value)
 {
-
+    m_timer->start(1000/value);
 }
+
 
 void Gui::on_blinkButton_clicked()
 {
-
+    m_blink_lauf = 1;
 }
+
 
 void Gui::on_lauflichtButton_clicked()
 {
+    m_blink_lauf = 0;
+    zaehler = 1;
+}
 
+
+void Gui::toggle()
+{
+    m_state = !m_state;
+
+    if(m_blink_lauf == 1)
+        {
+            if(m_state == 1)
+            {
+                zaehler = 15;
+
+            }else {
+                zaehler = 0;
+            }
+
+                m_leds->set(zaehler);
+            }
+
+        if(m_blink_lauf == 0)
+        {
+            m_leds->set(zaehler);
+                if(zaehler < 8)
+                    {
+                        zaehler = zaehler * 2;
+                    }
+                else {
+                        zaehler = 1;
+                    }
+        }
 }
